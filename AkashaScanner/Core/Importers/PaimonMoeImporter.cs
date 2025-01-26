f34901e60480269988862a4ea0d754ad1d822d69
Akrios <ghfelipe@hotmail.com>
2025-01-26 14:31:14 +0000

    refactoring: code improvement

---
+++ AkashaScanner/Core/Importers/PaimonMoeImporter.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace AkashaScanner.Core.Importers
+{
+    public static class PaimonMoeImporter
+    {
+      
+        public static bool Import(
+            string Import)
+        {
+            JObject obj = new();
+            if (!string.IsNullOrEmpty(Import))
+            {
+                var parsed = JToken.Parse(Import);
+                if (parsed is JObject ob)
+                {
+                    obj = ob;
+                }
+            }
+            return true;
+        }
+    }
+}
